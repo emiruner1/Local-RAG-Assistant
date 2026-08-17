99aa3dbbe77a3fe1d8ef8748fff3cb5b845eb23a
emiruner1 <emir.uner34@gmail.com>
2026-08-17 11:36:12 +0300

    Implement PDF loading functionality

--- utils/pdf_loader.py
+++ utils/pdf_loader.py
@@ -1,1 +1,14 @@
+from pypdf import PdfReader
 
+
+def load_pdf(file_path):
+    reader = PdfReader(file_path)
+
+    text = ""
+
+    for page in reader.pages:
+        content = page.extract_text()
+        if content:
+            text += content + "\n"
+
+    return text
