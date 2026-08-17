97eb265161983938a4ac537fc38b6c1e5c651fa7
emiruner1 <emir.uner34@gmail.com>
2026-08-17 11:37:49 +0300

    Implement chunk_text function for text chunking
    Add a function to split text into chunks of specified size.

--- utils/chunker.py
+++ utils/chunker.py
@@ -1,1 +1,11 @@
+def chunk_text(text, chunk_size=500):
+    chunks = []
 
+    start = 0
+
+    while start < len(text):
+        end = start + chunk_size
+        chunks.append(text[start:end])
+        start = end
+
+    return chunks
