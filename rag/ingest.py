d776057dd51d16122b5394454aa8c32bdef11436
emiruner1 <emir.uner34@gmail.com>
2026-08-18 09:35:28 +0300

    Add configurable chunk size to ingest function
    Updated chunk_text function to use configurable chunk size.

--- rag/ingest.py
+++ rag/ingest.py
@@ -2,6 +2,7 @@ import os
 
 from utils.pdf_loader import load_pdf
 from utils.chunker import chunk_text
+from config import CHUNK_SIZE
 
 from rag.embeddings import create_embedding, embedding_to_json
 
@@ -12,7 +13,7 @@ def ingest(pdf_path):
     create_table()
 
     text = load_pdf(pdf_path)
-    chunks = chunk_text(text)
+    chunks = chunk_text(text, chunk_size=CHUNK_SIZE)
 
     source = os.path.basename(pdf_path)
 
